bfad7fc5bca0ff288a73e243aa7164deefd24328
jepung <jeppung@gmail.com>
2025-08-06 15:17:34 +0700

    feat(app): adding tailwind, reconfigure project file, adding profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-w-screen min-h-screen text-slate-200 flex justify-center items-center">
+      <div className="flex flex-col items-center relative -top-10">
+        <div className="relative">
+          <img
+            src="/memoji.png"
+            className="text-center relative -bottom-5 z-0 pointer-events-none select-none"
+            draggable={false}
+          />
+          <div className="h-full -bottom-5 w-full absolute z-10 bg-linear-to-t from-[#242424] from-5%  to-transparent" />
+        </div>
+        <div className="text-center z-20">
+          <h1 className="text-4xl font-semibold">JEPPUNG</h1>
+          <p className="text-sm mt-1">Frontend Engineer | Software Engineer</p>
+          <div className="flex items-center justify-center gap-2 mt-4">
+            <a target="_blank" href="https://linkedin.com/in/jeppung">
+              <FaLinkedin size={30} />
+            </a>
+            <a target="_blank" href="https://github.com/jeppung">
+              <FaGithub size={30} />
+            </a>
+          </div>
+        </div>
       </div>
-      <h1>JEPPUNG</h1>
-    </>
+    </div>
   );
 }
 
